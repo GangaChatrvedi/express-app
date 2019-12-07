e1f7d2cb1d07bff1418644c5050022eb95cd2be0
GangaChatrvedi <ganga.chaturvedi24@gmail.com>
2019-12-07 15:43:03 +0530

    exp1

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 app = express();
 
 app.use(express.json());
 
-app.listen(8080, () =>{
-    console.log("running");
-})
-
-app.get('/',(req, res) => {
-    console.log(req.query)
+app.listen(8080, () => {
+  console.log("running");
 });
 
-
+app.get("/", (req, res) => {
+  res.send("hahahahah");
+});
